28badf86d1d3c84cc27366241d7532adfca97b3b
Wojciech Wojtyniak <4743+wojtyniak@users.noreply.github.com>
2024-12-04 19:27:20 -0800

    Day 03 - part 2

--- src/day03.lisp
+++ src/day03.lisp
@@ -2,15 +2,36 @@
   (:use :cl :advent :split-sequence))
 (in-package :advent.day03)
 
+(defparameter lines (load-input-lines 3))
+(defparameter input (load-input 3))
+
 (defparameter mul-pattern "mul\\((\\d{1,3}),(\\d{1,3})\\)")
 
 (defun find-groups (line)
   (ppcre:all-matches-as-strings mul-pattern line))
 
 
-
 (defun day03-part1 (lines)
   (let ((sum 0))
     (dolist (line lines)
       (ppcre:do-register-groups (a b) (mul-pattern line)
-        (setf sum (+ sum (* (parse-integer a) (parse-integer b))))))))
+        (setf sum (+ sum (* (parse-integer a) (parse-integer b))))))
+    sum))
+
+;;;; Part 2
+
+(defun filter-valid-substrings (out in next)
+  (case next
+    (:do
+     (multiple-value-bind (from to) (ppcre:scan "do\\(\\)" in)
+       (if to
+           (filter-valid-substrings out (subseq in to) :dont)
+           out)))
+    (:dont
+     (multiple-value-bind (from to) (ppcre:scan "don't\\(\\)" in)
+       (if from
+           (filter-valid-substrings (concatenate 'string out (subseq in 0 from)) (subseq in from) :do)
+           (concatenate 'string out in))))))
+
+(defun day03-part2 (input)
+  (day03-part1 (list (filter-valid-substrings "" input :dont))))
--- tests/day03.lisp
+++ tests/day03.lisp
@@ -1,13 +1,17 @@
 (defpackage :advent/tests.day03
   (:use :cl :fiveam :advent/tests)
   (:shadowing-import-from :advent.day03
-                          #:mul-pattern))
+                          #:mul-pattern
+                          #:find-groups
+                          #:filter-valid-substrings
+                          #:day03-part1
+                          #:day03-part2))
 ;; #:day03-part1
 ;; #:day03-part2
 
 (in-package :advent/tests.day03)
 (defvar ex1 "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
-
+(defvar ex2 "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
 
 (def-suite* day03 :in advent)
 (in-suite* day03)
